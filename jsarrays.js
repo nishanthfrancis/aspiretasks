b7e7add798c72e49454fbdbddb59d0da0d1cdfeb
nishanthfrancis <100779024+nishanthfrancis@users.noreply.github.com>
2024-03-28 14:16:30 +0530

    Add files via upload

--- jsarrays.js
+++ jsarrays.js
@@ -3,3 +3,18 @@ let fruits = ["apple", "banana", "orange"];
 console.log(fruits[0]); 
 
 fruits.push("grape"); console.log(fruits);   
+
+fruits.unshift('Pineapple'); console.log(fruits); 
+        
+fruits.pop(); console.log(fruits); 
+
+fruits.shift(); console.log(fruits); 
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+
+fruits.forEach(function(fruit) {
+    console.log(fruit);
+});
